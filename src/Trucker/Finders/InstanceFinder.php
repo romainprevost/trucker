bfdd9b95fa0077dca2bdd575756e60c148e41e88
Romain Prevost <romain.prevost@melusim.fr>
2015-01-09 00:57:22 +0100

    Headers parameter taken into account when fetching a model instance

--- src/Trucker/Finders/InstanceFinder.php
+++ src/Trucker/Finders/InstanceFinder.php
@@ -64,8 +64,8 @@ class InstanceFinder
         $request->createRequest(
             Config::get('request.base_uri'),
             UrlGenerator::getInstanceUri($model, [':id' => $id]),
-            'GET'
-            //Config::get('request.http_headers')
+            'GET',
+            Config::get('request.http_headers')
         );
 
         //add auth if it is needed
